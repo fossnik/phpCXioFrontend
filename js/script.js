4d6d0508d9f4ec662501b14dc431142d2c612099
Zack Hartmann <zackhartmann@gmail.com>
2018-03-12 22:29:24 -0400

    Implemented GetMarkets Endpoint Query

--- js/script.js
+++ js/script.js
@@ -2,8 +2,9 @@
 
 let orderbook = null;
 
-$('#pull').click(function() {
+$('#orderbook').click(function() {
 	let market = document.getElementById("market").value;
+
 	$.ajax({
 		type: 'POST',
 		url: 'php/pullJson.php',
@@ -11,43 +12,38 @@ $('#pull').click(function() {
 			market: market
 		},
 		success: function (JsonString) {
-			$('#status').html("pullJson.php Successfully Queried API");
 			orderbook = JSON.parse(JsonString);
+			printOrderBook(orderbook);
+			$('#status').html("pullJson.php Successfully Queried API" +
+				" endpoint 'market' #" + market + "\n" +
+				"request: " + orderbook.request);
 		}
 	});
-});
 
-$('#print').click(function() {
-	if (orderbook === null)
-		$('#status').html("Orderbook must first be Queried");
-	else
-		printOrderBook(orderbook)
-});
-
-function printOrderBook(json) {
-	// print table headers - BUY and SELL
-	$('#JS-fill').html(
-		"<tr>" +
-			"<th colspan='3' class='flex-item'>BUY</th>" +
-			"<th colspan='3' class='flex-item'>SELL</th>" +
-		"</tr>" +
-		"<tr>" +
-			"<th class='flex-item'>Order Time</th>" +
-			"<th class='flex-item'>Price</th>" +
-			"<th class='flex-item'>Quantity</th>" +
-
-			"<th class='flex-item'>Order Time</th>" +
-			"<th class='flex-item'>Price</th>" +
-			"<th class='flex-item'>Quantity</th>" +
-		"</tr>"
-	);
-
-	// fill table rows
-	let buyOrders = json.result.BuyOrders;
-	let sellOrders = json.result.SellOrders;
-	$.each(buyOrders, function(index, buyOrder) {
-		$('#JS-fill').append(
+	function printOrderBook(json) {
+		// print table headers - BUY and SELL
+		$('#JS-fill').html(
 			"<tr>" +
+			"<th colspan='3' class='flex-item buy'>BUY</th>" +
+			"<th colspan='3' class='flex-item sell'>SELL</th>" +
+			"</tr>" +
+			"<tr>" +
+			"<th class='flex-item buy'>Order Time</th>" +
+			"<th class='flex-item buy'>Price</th>" +
+			"<th class='flex-item buy'>Quantity</th>" +
+
+			"<th class='flex-item sell'>Order Time</th>" +
+			"<th class='flex-item sell'>Price</th>" +
+			"<th class='flex-item sell'>Quantity</th>" +
+			"</tr>"
+		);
+
+		// fill table rows
+		let buyOrders = json.result.BuyOrders;
+		let sellOrders = json.result.SellOrders;
+		$.each(buyOrders, function(index, buyOrder) {
+			$('#JS-fill').append(
+				"<tr>" +
 				"<td class='flex-item'>" + buyOrder.OrderTime + "</td>" +
 				"<td class='flex-item'>" + buyOrder.Price + "</td>" +
 				"<td class='flex-item'>" + buyOrder.Quantity + "</td>" +
@@ -55,7 +51,32 @@ function printOrderBook(json) {
 				"<td class='flex-item'>" + sellOrders[index].OrderTime + "</td>" +
 				"<td class='flex-item'>" + sellOrders[index].Price + "</td>" +
 				"<td class='flex-item'>" + sellOrders[index].Quantity + "</td>" +
-			"</tr>"
-		);
+				"</tr>"
+			);
+		});
+	}
+});
+
+$('#getmarkets').click(function() {
+
+	$.ajax({
+		type: 'POST',
+		url: 'php/pullJson.php',
+		data: {
+			endpoint: 'getmarkets'
+		},
+		success: function (JsonString) {
+			$('#status').html("pullJson.php Successfully Queried API Endpoint 'getMarkets'");
+			printGetMarkets(JsonString);
+			// getmarkets = JSON.parse(JsonString);
+			// printGetMarkets(getmarkets);
+		}
 	});
-}
+
+	function printGetMarkets(getmarkets) {
+		$('#log').html(getmarkets);
+		console.log(getmarkets);
+	}
+});
+
+
--- php/pullJson.php
+++ php/pullJson.php
@@ -1,19 +1,18 @@
 <?php
 
-	if (isset($_POST['market']))
+	if (isset($_POST['market'])) {
 		$MarketID = $_POST['market'];
-	else
-		$MarketID = 18;
+		$url = 'https://www.coinexchange.io/api/v1/getorderbook?market_id=' . $MarketID;
+		echo file_get_contents($url);
+	} else
+		if ($_POST['endpoint'] === 'getmarkets') {
+			echo file_get_contents('https://www.coinexchange.io/api/v1/getmarkets');
+		}
 
-	$url = 'https://www.coinexchange.io/api/v1/getorderbook?market_id=' . $MarketID;
 //	$url = '../json/.Sample-ltc-orderbook.json';
-
-	// pull down json
-	$htmlStr = file_get_contents($url);
 
 	// output api results as a file (not working)
 //	$fileName = './json/' . $MarketID . '-' . time() . '.json';
-//	echo file_put_contents($filename, $htmlStr);
+//	file_put_contents($filename, $htmlStr);
 
-	echo $htmlStr;
 ?>
