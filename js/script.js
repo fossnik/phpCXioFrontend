99bb512f0402e2779db32b4d15c16c87fb26cc0a
Zack Hartmann <zackhartmann@gmail.com>
2018-03-12 23:08:49 -0400

    Implemented GetMarkets Table

--- js/script.js
+++ js/script.js
@@ -14,9 +14,9 @@ $('#orderbook').click(function() {
 		success: function (JsonString) {
 			orderbook = JSON.parse(JsonString);
 			printOrderBook(orderbook);
-			$('#status').html("pullJson.php Successfully Queried API" +
-				" endpoint 'market' #" + market + "\n" +
-				"request: " + orderbook.request);
+			$('#status').html("pullJson.php Successfully Queried API<br>" +
+				"request: " + orderbook.request +
+				"  for  [ marketID:  " + market + " ]");
 		}
 	});
 
@@ -24,17 +24,17 @@ $('#orderbook').click(function() {
 		// print table headers - BUY and SELL
 		$('#JS-fill').html(
 			"<tr>" +
-			"<th colspan='3' class='flex-item buy'>BUY</th>" +
-			"<th colspan='3' class='flex-item sell'>SELL</th>" +
+				"<th colspan='3' class='flex-item buy'>BUY</th>" +
+				"<th colspan='3' class='flex-item sell'>SELL</th>" +
 			"</tr>" +
 			"<tr>" +
-			"<th class='flex-item buy'>Order Time</th>" +
-			"<th class='flex-item buy'>Price</th>" +
-			"<th class='flex-item buy'>Quantity</th>" +
+				"<th class='flex-item buy'>Order Time</th>" +
+				"<th class='flex-item buy'>Price</th>" +
+				"<th class='flex-item buy'>Quantity</th>" +
 
-			"<th class='flex-item sell'>Order Time</th>" +
-			"<th class='flex-item sell'>Price</th>" +
-			"<th class='flex-item sell'>Quantity</th>" +
+				"<th class='flex-item sell'>Order Time</th>" +
+				"<th class='flex-item sell'>Price</th>" +
+				"<th class='flex-item sell'>Quantity</th>" +
 			"</tr>"
 		);
 
@@ -44,13 +44,13 @@ $('#orderbook').click(function() {
 		$.each(buyOrders, function(index, buyOrder) {
 			$('#JS-fill').append(
 				"<tr>" +
-				"<td class='flex-item'>" + buyOrder.OrderTime + "</td>" +
-				"<td class='flex-item'>" + buyOrder.Price + "</td>" +
-				"<td class='flex-item'>" + buyOrder.Quantity + "</td>" +
+					"<td class='flex-item'>" + buyOrder.OrderTime + "</td>" +
+					"<td class='flex-item'>" + buyOrder.Price + "</td>" +
+					"<td class='flex-item'>" + buyOrder.Quantity + "</td>" +
 
-				"<td class='flex-item'>" + sellOrders[index].OrderTime + "</td>" +
-				"<td class='flex-item'>" + sellOrders[index].Price + "</td>" +
-				"<td class='flex-item'>" + sellOrders[index].Quantity + "</td>" +
+					"<td class='flex-item'>" + sellOrders[index].OrderTime + "</td>" +
+					"<td class='flex-item'>" + sellOrders[index].Price + "</td>" +
+					"<td class='flex-item'>" + sellOrders[index].Quantity + "</td>" +
 				"</tr>"
 			);
 		});
@@ -65,17 +65,46 @@ $('#getmarkets').click(function() {
 		data: {
 			endpoint: 'getmarkets'
 		},
-		success: function (JsonString) {
-			$('#status').html("pullJson.php Successfully Queried API Endpoint 'getMarkets'");
-			printGetMarkets(JsonString);
-			// getmarkets = JSON.parse(JsonString);
-			// printGetMarkets(getmarkets);
+		success: function (jsonString) {
+			getmarkets = JSON.parse(jsonString);
+			$('#status').html("pullJson.php Successfully Queried API<br>" +
+				"request: " + getmarkets.request);
+			printGetMarkets(getmarkets);
 		}
 	});
 
 	function printGetMarkets(getmarkets) {
-		$('#log').html(getmarkets);
-		console.log(getmarkets);
+		// print table headers
+		$('#JS-fill').html(
+			"<tr>" +
+				"<th class='flex-item'>Active</th>" +
+				"<th class='flex-item'>BaseCurrency</th>" +
+				"<th class='flex-item'>BaseCurrencyCode</th>" +
+				"<th class='flex-item'>BaseCurrencyID</th>" +
+				"<th class='flex-item'>MarketAssetCode</th>" +
+				"<th class='flex-item'>MarketAssetID</th>" +
+				"<th class='flex-item'>MarketAssetName</th>" +
+				"<th class='flex-item'>MarketAssetType</th>" +
+				"<th class='flex-item'>MarketID</th>" +
+			"</tr>"
+		);
+
+		// fill table
+		$.each(getmarkets.result, function(index, market) {
+			$('#JS-fill').append(
+				"<tr>" +
+					"<td class='flex-item'>" + market.Active +
+					"<td class='flex-item'>" + market.BaseCurrency +
+					"<td class='flex-item'>" + market.BaseCurrencyCode +
+					"<td class='flex-item'>" + market.BaseCurrencyID +
+					"<td class='flex-item'>" + market.MarketAssetCode +
+					"<td class='flex-item'>" + market.MarketAssetID +
+					"<td class='flex-item'>" + market.MarketAssetName +
+					"<td class='flex-item'>" + market.MarketAssetType +
+					"<td class='flex-item'>" + market.MarketID +
+				"</tr>"
+			);
+		});
 	}
 });
 
